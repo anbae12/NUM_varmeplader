aa8a3e14d2e39de54c0ce7cf27aee402f35602d7
Anders Launer Bæk <anbae12@student.sdu.dk>
2015-03-25 08:34:57 +0100

    Some data input.

--- Varmeplader/Varmeplader/main.cpp
+++ Varmeplader/Varmeplader/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include "nr3.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// DATA
+Doub T1 = 1000;
+Doub T2 = 500;
+Doub epsilon1 = 0.80;
+Doub epsilon2 = 0.60;
+Doub sigma = 1.712*10e-0;
+Doub d = 1.00;
+Doub w = 1.00;
+
+
+int main() {
+    
+    
+    
+    
+    
+    
     return 0;
 }
