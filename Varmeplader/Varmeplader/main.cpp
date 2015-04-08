d49164262d6189194d63746be48672cf55bc2718
Anders Launer Bæk <anbae12@student.sdu.dk>
2015-04-08 10:54:39 +0200

    Anders Update

--- Varmeplader/Varmeplader/main.cpp
+++ Varmeplader/Varmeplader/main.cpp
@@ -44,52 +44,94 @@ struct Funcd {
 Doub make_h(int iteratations, Doub llim, Doub ulim){
     return (ulim-llim)/iteratations;
 }
-MatDoub make_A(int iteratations, Doub XXX, Doub YYY, Doub h){
-    int rows = (iteratations+1)*2;
-    int cols = 2;
-    int tempH1 = 0;
-    int tempH2 = 0;
-    MatDoub A(rows,cols);
-    for (int i=0; i<cols; i++) {
-        for (int j=0; j<rows; j++) {
-            if (i==0) {
-                if (j<iteratations+1) {
-                    A[j][i] = 0;
-                }
-                if (j==iteratations+1) {
-                    
-                    A[j][i] = 0.5*Func(XXX,YYY);
-                }
-                if (j>iteratations+1 && j<rows-1) {
-                    ++tempH1;
-                    A[j][i] = Func(XXX,YYY+(h*tempH1));
-                }
-                if (j==rows-1) {
-                    ++tempH1;
-                    A[j][i] = 0.5*Func(XXX,YYY+(h*tempH1));
-                }
-            }
-            if (i==1) {
-                if (j>iteratations) {
-                    A[j][i] = 0;
-                }
-                if (j==0) {
-                    A[j][i] = 0.5*Func(XXX, YYY);
-                }
-                if (j>0 && j<iteratations) {
-                    ++tempH2;
-                    A[j][i] = Func(XXX,YYY+(h*tempH2));
-                }
-                if (j==iteratations) {
-                    ++tempH2;
-                    A[j][i] = 0.5*Func(XXX,YYY+(h*tempH2));
-                }
-            }
+MatDoub make_A(int x, Doub XXX, Doub YYY, Doub h){
+      int rows = (iteratations+1)*2;
+        int cols = 2;
+        int tempH1 = 0;
+        int tempH2 = 0;
+    //
+    //
+    //    MatDoub A(rows,cols);
+    //    for (int i=0; i<cols; i++) {
+    //        for (int j=0; j<rows; j++) {
+    //            if (i==0) {
+    //                if (j<iteratations+1) {
+    //                    A[j][i] = 0;
+    //                }
+    //                if (j==iteratations+1) {
+    //
+    //                    A[j][i] = 0.5*Func(XXX,YYY);
+    //                }
+    //                if (j>iteratations+1 && j<rows-1) {
+    //                    ++tempH1;
+    //                    A[j][i] = Func(XXX,YYY+(h*tempH1));
+    //                }
+    //                if (j==rows-1) {
+    //                    ++tempH1;
+    //                    A[j][i] = 0.5*Func(XXX,YYY+(h*tempH1));
+    //                }
+    //            }
+    //            if (i==1) {
+    //                if (j>iteratations) {
+    //                    A[j][i] = 0;
+    //                }
+    //                if (j==0) {
+    //                    A[j][i] = 0.5*Func(XXX, YYY);
+    //                }
+    //                if (j>0 && j<iteratations) {
+    //                    ++tempH2;
+    //                    A[j][i] = Func(XXX,YYY+(h*tempH2));
+    //                }
+    //                if (j==iteratations) {
+    //                    ++tempH2;
+    //                    A[j][i] = 0.5*Func(XXX,YYY+(h*tempH2));
+    //                }
+    //            }
+    //        }
+    //    }
+    //    return A;
+    int cols = 12;
+    int rows = 12;
+    
+    
+    MatDoub A(cols,rows);
+    //    A[0][0] = 0; A[0][1] = 0; A[0][2] = 0; A[0][3] = 0; A[0][4] = 0; A[0][5] = 0; A[0][6] = 0; A[0][7] = 0; A[0][8] = 0; A[0][9] = 0;
+    //    A[1][0] = 0; A[1][1] = 0; A[1][2] = 0; A[1][3] = 0; A[1][4] = 0; A[1][5] = 0; A[1][6] = 0; A[1][7] = 0; A[1][8] = 0; A[1][9] = 0;
+    //    A[2][0] = 0; A[2][1] = 0; A[2][2] = 0; A[2][3] = 0; A[2][4] = 0; A[2][5] = 0; A[2][6] = 0; A[2][7] = 0; A[2][8] = 0; A[2][9] = 0;
+    //    A[3][0] = 0; A[3][1] = 0; A[3][2] = 0; A[3][3] = 0; A[3][4] = 0; A[3][5] = 0; A[3][6] = 0; A[3][7] = 0; A[3][8] = 0; A[3][9] = 0;
+    //    A[4][0] = 0; A[4][1] = 0; A[4][2] = 0; A[4][3] = 0; A[4][4] = 0; A[4][5] = 0; A[4][6] = 0; A[4][7] = 0; A[4][8] = 0; A[4][9] = 0;
+    //    A[5][0] = 0; A[5][1] = 0; A[5][2] = 0; A[5][3] = 0; A[5][4] = 0; A[5][5] = 0; A[5][6] = 0; A[5][7] = 0; A[5][8] = 0; A[5][9] = 0;
+    //    A[6][0] = 0; A[6][1] = 0; A[6][2] = 0; A[6][3] = 0; A[6][4] = 0; A[6][5] = 0; A[6][6] = 0; A[6][7] = 0; A[6][8] = 0; A[6][9] = 0;
+    //    A[7][0] = 0; A[7][1] = 0; A[7][2] = 0; A[7][3] = 0; A[7][4] = 0; A[7][5] = 0; A[7][6] = 0; A[7][7] = 0; A[7][8] = 0; A[7][9] = 0;
+    //    A[8][0] = 0; A[8][1] = 0; A[8][2] = 0; A[8][3] = 0; A[8][4] = 0; A[8][5] = 0; A[8][6] = 0; A[8][7] = 0; A[8][8] = 0; A[8][9] = 0;
+    //    A[9][0] = 0; A[9][1] = 0; A[9][2] = 0; A[9][3] = 0; A[9][4] = 0; A[9][5] = 0; A[9][6] = 0; A[9][7] = 0; A[9][8] = 0; A[9][9] = 0;
+    //
+    
+    
+    int i = 0;
+    for (int j = 0; j<rows; j++) {
+        A[j][i]=1;
+        i++;
+    }
+    
+    
+    for (int i = cols/2; i<cols; i++) {
+        for (int j = 0; j<rows/2; j++) {
+            A[j][i]=9;
+            
         }
     }
+    for (int i = 0; i<cols/2; i++) {
+        for (int j = rows/2; j<rows; j++) {
+            A[j][i]=8;
+            
+        }
+    }
+    
+    
     return A;
 }
-// MIKKEL Trapetz
+
 
 
 
@@ -116,16 +158,16 @@ int main() {
     
     std::cout<<AA<<std::endl;
     
-    b[0] = (-epsilon1*sigma*pow(T1,4))/((1-epsilon1)*h);
-    b[1] = (-epsilon2*sigma*pow(T2,4))/((1-epsilon2)*h);
-    
-    std::cout<<b<<std::endl;
-    
-    
-    
-    
-    
-    SVD obj(AA);
+    //    b[0] = (-epsilon1*sigma*pow(T1,4))/((1-epsilon1)*h);
+    //    b[1] = (-epsilon2*sigma*pow(T2,4))/((1-epsilon2)*h);
+    //
+    //    std::cout<<b<<std::endl;
+    //
+    //
+    //
+    //
+    //
+    //    SVD obj(AA);
     //    VecDoub W=obj.w;
     //    MatDoub V=obj.v;
     //    MatDoub U=obj.u;
